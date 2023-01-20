ffa0945e5319d2aa55bc4123f8c2fb648cef306d
FAMASoon <famasoon@gmail.com>
2023-01-20 21:51:42 +0900

    retrieving user information with ldap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use ldap3::*;
+
 fn main() {
-    println!("Hello, world!");
+    let ldap = LdapConn::new("ldap://192.168.0.100:3268");
+    let mut ldap_con = match ldap {
+        Ok(l) => l,
+        Err(e) => panic!("{}", e),
+    };
+
+    ldap_con.simple_bind("CN=Administrator,CN=Users,DC=tech69,DC=local", "Passw0rd").unwrap();
+
+    let res = ldap_con.search("DC=tech69,DC=local", Scope::Subtree, "(objectclass=user)", vec!["dn"]).unwrap();
+
+    let (re, _) = res.success().unwrap();
+
+    for i in re {
+        println!("{}", SearchEntry::construct(i).dn);
+    }
 }
